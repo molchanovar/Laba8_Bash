d5046851987853e6f83756c8047bb5003ad8d4f9
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-01-26 00:34:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,12 @@
 more /home/ubuntupc/Downloads/access.log | awk '{print $1}' | sort | uniq -c | sort -rn | head -10
 
 # Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-
+more /home/ubuntupc/Downloads/access.log | awk '/ HTTP/ {print $7}' | sort | uniq -c | sort -rn | head -10
 
 # все ошибки c момента последнего запуска
-
+more /home/ubuntupc/Downloads/access.log | awk '/ HTTP/ {print $9}' | sort | uniq -c | sort -rn
 
 # список всех кодов возврата с указанием их кол-ва с момента последнего запуска
+
 
 
