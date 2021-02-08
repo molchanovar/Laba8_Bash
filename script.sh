c212f67fa917a6b0d317ca17e84973cc19be4c8e
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-02-08 13:20:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,9 @@ num=$(head -1 $HOME/Downloads/CountLines.txt)
 
 echo "File $logfile has $num lines in it"
 
+# Multi-start protection - switch ON
+set -o noclobber
+
 # 1. Top 10 IP with Req (1-line number, 2-IP, 3-Count)
 echo "Top 10 IP with count of Request"
 awk 'NR > '$num' {print $1}' $logfile | sort | uniq -c | sort -rn | head -10 | awk 'BEGIN {print "Number IP COUNT"} {print FNR,$2,$1}' | column -t
@@ -33,6 +36,9 @@ echo "All response code from last start"
 awk 'NR > '$num'' $logfile | awk '/HTTP/ {print $9}' | sort | uniq -c | sort -nr | awk 'BEGIN {print "COUNT RESPONSE"} {print $1,$2}' | column -t
 
 echo "---------------------------------------"
+
+# Multi-start protection - switch OFF
+set +o noclobber
 
 # 5. Count of lines in this moment 
 echo "Lines was counted in CountLines.txt"
